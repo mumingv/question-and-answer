1fef18d627c7cd62ba1329a393ee76d7c9e26754
mumingv <mumingv@163.com>
2019-06-06 20:21:03 +0800

    阮一峰 - Generator 函数的含义与用法

--- 2019-06-04.nodejs/article-2-ruanyifeng-generator/0C_coroutine_01.js
+++ 2019-06-04.nodejs/article-2-ruanyifeng-generator/0C_coroutine_01.js
@@ -8,27 +8,48 @@
  * 第四步，协程A恢复执行。
  */
 
-const fs = require('fs');
 var readFile = require('fs-readfile-promise');
 
 // 依次读取并处理三个文件
-function asyncJob() {
-    var data = yield fs.readFile('/etc/passwd');
-    // var data = yield readFile('/etc/passwd');
+function* asyncJob() {
+    var data = yield readFile('/etc/passwd');
+    console.log('--- asyncJob /etc/passwd ---');
+    console.log(data);
+
+    data = yield readFile('/etc/profile');
+    console.log('--- asyncJob /etc/profile ---');
+    console.log(data);
+
+    data = yield readFile('/etc/ttys');
+    console.log('--- asyncJob /etc/ttys ---');
     console.log(data);
 }
 
-// fs.readFile('/etc/passwd', function (err, data) {
-//     if (err) throw err;
-//     console.log(data);
-//     fs.readFile('/etc/profile', function (err, data) {
-//         if (err) throw err;
-//         console.log(data);
-//         fs.readFile('/etc/ttys', function (err, data) {
-//             if (err) throw err;
-//             console.log(data);
-//         });
-//     });
-// });
+var g = asyncJob();
+console.log(g);  // Object [Generator] {}
+
+// 第1个文件：/etc/passwd
+var resultFile1 = g.next();
+console.log(resultFile1);  // { value: Promise { <pending> }, done: false }
+
+resultFile1.value.then(function(data) {
+    // console.log('-------');
+    // console.log(data);  // <Buffer 23 23 0a 23 20 55 73 65 72 20 44 61 74 61 62 61 73 65 0a 23 20 0a 23 20 4e 6f 74 65 20 74 68 61 74 20 74 68 69 73 20 66 69 6c 65 20 69 73 20 63 6f 6e ... 6724 more bytes>
+    // console.log(data.toString());  // 文件内容
+    // console.log('-------');
+    
+    // 第2个文件
+    var resultFile2 = g.next(data.toString());
+    console.log(resultFile2);  // { value: Promise { <pending> }, done: false }
+    resultFile2.value.then(function(data) {
+        // 第3个文件
+        var resultFile3 = g.next(data.toString());
+        console.log(resultFile3);  // { value: Promise { <pending> }, done: false }
+        resultFile3.value.then(function(data) {
+            g.next(data.toString());
+        });
+    });
+});
+
 console.log("finish");
 
