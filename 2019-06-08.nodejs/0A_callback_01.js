f86bf64ac70c95f33cf17f0e6a74b4902816c46f
mumingv <mumingv@163.com>
2019-06-08 13:50:38 +0800

    Node.js异步处理的各种写法

---
+++ 2019-06-08.nodejs/0A_callback_01.js
@@ -0,0 +1,19 @@
+/**
+ * 异步处理方式：回调函数
+ */
+
+const fs = require('fs');
+
+fs.readFile('Jay.txt', 'utf8', function (err, data) {
+    if (err) throw err;
+    console.log(data);
+    fs.readFile('Angela.txt', 'utf8', function (err, data) {
+        if (err) throw err;
+        console.log(data);
+        fs.readFile('Henry.txt', 'utf8', function (err, data) {
+            if (err) throw err;
+            console.log(data);
+        });
+    });
+});
+console.log("finish");
