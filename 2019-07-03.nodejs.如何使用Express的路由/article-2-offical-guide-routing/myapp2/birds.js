3fd09fe57cc299571b6481a908395a1abb3e2a3a
mumingv <mumingv@163.com>
2019-07-03 20:51:39 +0800

    nodejs.如何使用Express的路由

--- 2019-07-03.nodejs.如何使用Express的路由/article-2-offical-guide-routing/myapp2/birds.js
+++ 2019-07-03.nodejs.如何使用Express的路由/article-2-offical-guide-routing/myapp2/birds.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-// middleware that is specific to this router
+// 中间件函数（可选）
 router.use(function timeLog(req, res, next) {
     console.log('Time: ', Date.now());
     next();
 });
-// define the home page route
+
 router.get('/', function(req, res) {
     res.send('Birds home page');
 });
-// define the about route
+
 router.get('/about', function(req, res) {
     res.send('About birds');
 });
