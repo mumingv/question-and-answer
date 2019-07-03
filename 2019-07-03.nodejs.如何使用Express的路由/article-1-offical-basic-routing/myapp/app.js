f3094a9fc464a9d57fb2a0e16959840c7572310a
mumingv <mumingv@163.com>
2019-07-03 15:16:42 +0800

    nodejs.如何使用Express的路由

--- 2019-07-03.nodejs.如何使用Express的路由/article-1-offical-basic-routing/myapp/app.js
+++ 2019-07-03.nodejs.如何使用Express的路由/article-1-offical-basic-routing/myapp/app.js
@@ -6,7 +6,18 @@ app.get("/", function(req, res) {
     res.send("Hello World!");
 });
 
+app.post("/", function(req, res) {
+    res.send("Got a POST request at /");
+});
+
+app.put("/user", function(req, res) {
+    res.send("Got a PUT request at /user");
+});
+
+app.delete("/user", function(req, res) {
+    res.send("Got a DELETE request at /user");
+});
+
 app.listen(3000, function() {
     console.log("Listening on port 3000!");
 });
-
